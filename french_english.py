e3654927f8ff47efb4bafbcc3375109c1377b6d8
angelh187 <angelh187@nycstudents.net>
2026-03-03 10:17:12 -0500

    Update french_english.py

--- french_english.py
+++ french_english.py
@@ -19,7 +19,7 @@
         language(100, "The red cat sat on the mat.")
  """
 
-def lang(sent):
+""" def lang(sent):
     s = 0
     t = 0
     for i in sent:
@@ -36,5 +36,51 @@ def lang(sent):
 
 
 lang("why are you so sad cat?")
-lang("this is a ttttttest")
-    
+lang("this is a ttttttest") """
+
+
+
+
+ 
+""" def honi(x):
+    h = 0
+    o = 0
+    n = 0
+    e = 0
+    honis = 0
+    for i in x:
+        if i == "h" or "H":
+            h = h +1
+        elif i == "o" or "O":
+            o = o +1 
+        elif i == "N" or "n":
+            n = n +1
+        else:
+            i == "i" or "I"
+            e = e + 1
+        if (h + o + n + e) / 4 == 0:
+            honis == (h + o + n + e) / 4
+
+    honi("honihoni")
+    print(honis) """
+def honi(sent):
+    h = 0 
+    for i in sent:
+        if i == "h" or "H":
+            h = h +1
+        elif i == "o" or "O":
+            h = h +1
+        elif i == "N" or "n":
+            h = h +1
+        else:
+            i == "i" or "I"
+            h = h +1
+            def honis(x):
+                x== h/4
+honiOHODNIHODNIK")
+
+        
+
+
+
+
