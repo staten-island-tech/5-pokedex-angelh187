d6232fb1ae0c808a51b93a8f61b2ef63a1607f46
angelh187 <angelh187@nycstudents.net>
2026-03-09 10:17:54 -0400

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -4,6 +4,10 @@ pokedex = open("./pokedex.json", encoding="utf8")
 ## create variable "data" that represents the enitre pokedex list
 data = json.load(pokedex)
 
-for index, item in enumerate("./pokedex.json"):
+""" for index, item in enumerate("./pokedex.json"):
     print("Item#: ",index) 
-    print(item["name"])
+    print(item["name"]) """
+
+lang = (input("Please select a language"))
+for item in data:
+    print(item["name"][lang])
